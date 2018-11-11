e43a204603b61dc74029f629c93ae0d8547d692a
tsantos <tales.augusto.santos@gmail.com>
2018-11-11 17:37:24 -0200

    Normaliza objeto `Amount`

--- src/Controller/V2/InvoiceController.php
+++ src/Controller/V2/InvoiceController.php
@@ -4,6 +4,7 @@ namespace App\Controller\V2;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use TSantos\Serializer\SerializerInterface;
 
 /**
  * Invoice Controller
@@ -13,11 +14,35 @@ use Symfony\Component\HttpFoundation\JsonResponse;
 class InvoiceController extends Controller
 {
     /**
+     * @var SerializerInterface
+     */
+    private $serializer;
+
+    /**
+     * InvoiceController constructor.
+     * @param SerializerInterface $serializer
+     */
+    public function __construct(SerializerInterface $serializer)
+    {
+        $this->serializer = $serializer;
+    }
+
+    /**
      * @return JsonResponse
      */
     public function listAction(): JsonResponse
     {
-        return new JsonResponse(['time' => time()]);
+        try {
+            $service = $this->get('app.infrastructure.service.invoice');
+            $data = $service->invoiceList();
+
+            return new JsonResponse($this->serializer->normalize($data));
+        } catch (\Throwable $e) {
+            return new JsonResponse(
+                $e->getMessage(),
+                JsonResponse::HTTP_INTERNAL_SERVER_ERROR
+            );
+        }
     }
 
     /**
